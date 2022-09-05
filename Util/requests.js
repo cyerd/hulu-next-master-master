18f013df113ef38c65ee1b65624c568c20058117
Hudheyfa Cyerd <hudheyfa@gmail.com>
2022-09-05 17:58:19 +0300

    updated env variable

--- Util/requests.js
+++ Util/requests.js
@@ -1,5 +1,6 @@
 
-const API_KEY = process.env.API_KEY;
+// const API_KEY = process.env.API_KEY;
+const API_KEY = "ac5ad5908e61a2da0f0ce2825d3b2788";
 
 // export default {
 //   fetchTrending: `/trending/all/week?api_key=${API_KEY}& language=en-US`,
