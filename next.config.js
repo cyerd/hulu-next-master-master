675b76f0b8799565d34219fb7eebb91ea5e8e097
Hudheyfa Cyerd <hudheyfa@gmail.com>
2023-07-06 14:32:13 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@ module.exports = {
   images: {
     domains: ["image.tmdb.org"],
   },
-  future: {
-    webpack5: true,
-  },
-  webpack: function (config, options) {
-    console.log(options.webpack.version); // Should be webpack v5 now
-    config.experiments = {};
-    return config;
-  },
+  // future: {
+  //   webpack5: true,
+  // },
+  // webpack: function (config, options) {
+  //   console.log(options.webpack.version); // Should be webpack v5 now
+  //   config.experiments = {};
+  //   return config;
+  // },
 };
